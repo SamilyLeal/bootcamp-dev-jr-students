1e98f78263c574154a4921aaafc4a5b4953b6f18
SamilyLeal <lealsamilysuga@gmail.com>
2024-03-31 12:20:36 -0300

    closes #1 - backend initial setup

---
+++ backend/src/main/java/com/ssl/bootcampdevjrstudents/BootcampDevJrStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.ssl.bootcampdevjrstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampDevJrStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampDevJrStudentsApplication.class, args);
+	}
+
+}
