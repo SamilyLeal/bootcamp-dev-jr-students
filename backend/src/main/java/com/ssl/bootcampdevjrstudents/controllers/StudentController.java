efb738a4233c6a02849736039f6786a764ae61e1
SamilyLeal <lealsamilysuga@gmail.com>
2024-03-31 12:33:13 -0300

    closes #3 - add student controller

---
+++ backend/src/main/java/com/ssl/bootcampdevjrstudents/controllers/StudentController.java
@@ -0,0 +1,58 @@
+package com.ssl.bootcampdevjrstudents.controllers;
+
+import com.ssl.bootcampdevjrstudents.models.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+
+    List<Student> students = new ArrayList<>();
+
+    @PostConstruct
+    public void init(){
+        students.add(new Student(1, "octopus", "oct@gmail.com",
+                            "11944556677",2,1));
+
+        students.add(new Student(2, "shrimp", "shr@gmail.com",
+                "11944556699",2,2));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents(){
+        return ResponseEntity.ok(students);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable int id){
+        Student student = students.stream()
+                .filter(s -> s.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found with the given id: "+ id));
+
+        return ResponseEntity.ok(student);
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> save(@RequestBody Student student){
+        student.setId(students.size() + 1);
+        students.add(student);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(student.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(student);
+    }
+}
